3293ef9c85609c5b677fbfb4b8138d7efbd9ffef
Bhupeshverma <bhupesh.v94@gmail.com>
2019-10-02 02:00:21 +0530

    Updates

---
+++ precache-manifest.14766e1002dfd88ca886cd1735efcb02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed86e7fde5b2fbd6f7b379423be52c5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "b687617d559baa4d109a",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "3463f1efc2638610ec2a",
+    "url": "/robofriends/static/css/main.dcdc5e06.chunk.css"
+  },
+  {
+    "revision": "b687617d559baa4d109a",
+    "url": "/robofriends/static/js/2.80796f17.chunk.js"
+  },
+  {
+    "revision": "3463f1efc2638610ec2a",
+    "url": "/robofriends/static/js/main.116a1584.chunk.js"
+  },
+  {
+    "revision": "ef7ed2c3e67d53841831",
+    "url": "/robofriends/static/js/runtime-main.ed74653d.js"
+  },
+  {
+    "revision": "9ddff8f2d9507a93a56e43a7a36c6ac4",
+    "url": "/robofriends/static/media/SEGA.9ddff8f2.woff"
+  }
+]);
